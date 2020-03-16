06cdbbc85d544c70afc91b1f69c541596244e192
Yevhenii <yevhenii1709@gmail.com>
2020-03-16 18:52:37 +0200

    create div

--- js/script2.js
+++ js/script2.js
@@ -2,6 +2,6 @@
 let age = document.getElementById('age');
 
 function showUser(surname, name) {
-	alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+	alert(`Пользователь ${surname} ${name}, его возраст ${this.value}`);
 }
 showUser.apply(age, ["Иванов", "Иван"]);
